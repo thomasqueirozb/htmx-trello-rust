6195bdb1cc5899e21fb7e51048654806771c4ba1
Thomas Queiroz <thomasqueirozb@gmail.com>
2023-12-12 00:31:48 -0500

    db: add QueryId & QueryIds traits

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,56 @@
+use crate::util::{Helper, InIndexVector};
+use crate::{db, models};
+
+use actix_web::Result as AwResult;
+use async_trait::async_trait;
 use serde::Serialize;
-use sqlx::FromRow;
 use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+use sqlx::{FromRow, Pool, QueryBuilder};
+
+type DB = sqlx::Sqlite;
+type DBPool = Pool<DB>;
+
+#[async_trait]
+pub trait QueryId {
+    async fn query_id(id: i64, db: &DBPool) -> AwResult<Self>
+    where
+        Self: Sized;
+}
+
+#[async_trait]
+pub trait QueryIds {
+    async fn query_ids(ids: &[i64], db: &DBPool) -> AwResult<Vec<Self>>
+    where
+        Self: Sized;
+}
+
+#[async_trait]
+impl QueryId for models::BoardData {
+    async fn query_id(id: i64, db: &DBPool) -> AwResult<Self> {
+        Ok(
+            sqlx::query_as!(db::Board, "SELECT * FROM boards where id = ?", id)
+                .fetch_one(db)
+                .await
+                .ensure_data_type()?
+                .into(),
+        )
+    }
+}
+
+#[async_trait]
+impl QueryIds for models::ListData {
+    async fn query_ids(ids: &[i64], db: &DBPool) -> AwResult<Vec<Self>> {
+        Ok(QueryBuilder::new("SELECT * FROM lists WHERE id")
+            .in_index_vector(ids)
+            .build_query_as::<db::List>()
+            .fetch_all(db)
+            .await
+            .ensure_data_type()?
+            .into_iter()
+            .map(Self::from)
+            .collect())
+    }
+}
 
 #[derive(Serialize, FromRow, Debug, Clone)]
 pub struct Card {
