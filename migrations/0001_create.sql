2a806ac49e01d4db2053c8e99b40f709ad866437
Thomas Queiroz <thomasqueirozb@gmail.com>
2024-01-22 22:11:28 -0500

    migrations: change type TEXT -> JSON

--- migrations/0001_create.sql
+++ migrations/0001_create.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS boards (
     id INTEGER PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
-    lists_order TEXT DEFAULT "[]" NOT NULL
+    lists_order JSON DEFAULT "[]" NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS lists (
     id INTEGER PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
-    cards_order TEXT DEFAULT "[]" NOT NULL
+    cards_order JSON DEFAULT "[]" NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cards (
